fc2c3dfdb4a56baa1a40a8bc7cade12f8acde472
Karl Pfister <kpfister@me.com>
2024-04-12 11:35:59 -0600

    Initial commit

---
+++ Breezy/BreezyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezyApp.swift
+//  Breezy
+//
+//  Created by Karl Pfister on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BreezyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
